ced99d5d6be24a529a206ec12fdfa5a063a9453e
Dean Congin <dcongin@Deans-MacBook-Pro.local>
2020-05-27 17:09:38 -0400

    init

---
+++ testappmac/Controllers/StupidController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace testappmac.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StupidController : MyControllerBase
+    {
+        
+
+        private readonly ILogger<StupidController> _logger;
+
+        public StupidController(ILogger<StupidController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+
+            return "Stupid Controller Does Nothing";
+        }
+    }
+}
